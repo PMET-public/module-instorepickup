3a9020d821d9064fc1e57794860b6a538827adb7
Jeff Britts <jbritts@adobe.com>
2019-07-26 09:29:41 -0400

    fix for b2b

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,7 +2,9 @@ var config = {
     "map": {
         "*": {
             magentoeseInStorePickupStoreSelector: 'MagentoEse_InStorePickup/js/store_selector',
-            inStorePickupProduct: 'MagentoEse_InStorePickup/js/in-store-pickup-product'
+            inStorePickupProduct: 'MagentoEse_InStorePickup/js/in-store-pickup-product',
+            'Magento_NegotiableQuote/template/checkout/summary/cart-items.html': 'MagentoEse_InStorePickup/template/summary/cart-items.html'
         }
+
     }
 };
